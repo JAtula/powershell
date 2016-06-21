5db6659e88c0d54c564d88e5c5ed32eea82cf417
Juhani Atula <juhaniatula@gmail.com>
2016-06-21 08:16:55 +0300

    first

---
+++ Connect-ExchangeOnline/Connect-ExchangeOnline.psm1
@@ -0,0 +1,90 @@
+﻿<#
+.Synopsis
+   Connect to Exchange Online. 
+.EXAMPLE
+    PS C:\> Connect-ExchangeOnline -emailAddress admin@company.onmicrosoft.com
+    WARNING: The names of some imported commands from the module 'tmp_3cjh1l1z.bfp' include unapproved verbs that 
+    might make them less discoverable. To find the commands with unapproved verbs, run the Import-Module command a
+    gain with the Verbose parameter. For a list of approved verbs, type Get-Verb.
+
+    ModuleType Version    Name                                ExportedCommands                                   
+    ---------- -------    ----                                ----------------                                   
+    Script     1.0        tmp_3cjh1l1z.bfp                    {Add-AvailabilityAddressSpace, Add-DistributionG...
+
+    Connected to Microsoft.Exchange
+#>
+function Connect-ExchangeOnline
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Emailaddress input
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        [string]$emailAddress
+
+    )
+
+    Begin
+    {
+        [string]$mail = ""
+        
+        #Get user pwd.
+        Write-Verbose "Input password"
+        [System.Security.SecureString]$secString = Read-Host 'Password' -AsSecureString
+        $OldPSSession = Get-PSSession | Where-Object {$_.ConfigurationName -eq "Microsoft.Exchange"}
+    }
+    Process
+    {
+        #Remove existing session.
+        Write-Verbose "Remove existing session"
+        if($OldPSSession -ne $null){
+            Remove-PSSession -Id $OldPSSession.Id -ErrorAction SilentlyContinue
+        }
+        
+        try
+        {
+            #Test emailaddress validity.
+            Write-Verbose "Test email address validity"
+            $mail = New-Object System.Net.Mail.MailAddress($emailAddress) -ErrorAction stop
+        }
+        catch
+        {
+            Write-Host "Mail format invalid." -ForegroundColor Red
+            return
+        }
+        
+        $cred = New-Object System.Management.Automation.PSCredential($mail,$secString)
+
+        try{
+            #Connect MSOL service.
+            Write-Verbose "Connect MSOL service"
+            Connect-MsolService -Credential $cred -ErrorAction Stop | Out-Null
+        }
+        catch{
+            Write-Host $Error[0].Exception.Message -ForegroundColor Red
+            return
+        }
+
+        try{
+            #Connect to Exchange Online
+            Write-Verbose "Connect to Exchange Online"
+            $PSSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/" -Credential $cred -Authentication Basic -AllowRedirection -ErrorAction Stop 
+            Import-Module (Import-PSSession $PSSession -AllowClobber) -Global | Out-Null 
+        }
+        catch{
+            write-host $Error[0].Exception.Message -ForegroundColor Red
+            return
+        }
+       
+    }
+    End
+    {
+        if($PSSession -ne $null){
+            Write-Host "`nConnected to $($PSSession.ConfigurationName)" -ForegroundColor Yellow
+        }else{
+            Write-Host "Session import failed." -ForegroundColor Red
+        }
+    }
+}
---
+++ Connect-SharepointOnline/Connect-SharepointOnline.psm1
@@ -0,0 +1,98 @@
+﻿<#
+.Synopsis
+   Connect to SharePoint Online. 
+.EXAMPLE
+   
+#>
+function Connect-SharePointOnline
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Emailaddress input
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        [string]$emailAddress,
+
+
+         # Tenant name
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   Position=1)]
+        [string]$tenant
+
+    )
+
+    Begin
+    {
+        [string]$mail = ""
+
+        #Import Sharepoint Online module.
+        Write-Verbose "Importing Sharepoint Online module.."
+        if(!(Get-Module).Name -ne "Microsoft.Online.Sharepoint.Powershell"){
+            try{
+                Import-Module 'C:\Program Files\SharePoint Online Management Shell\Microsoft.Online.SharePoint.PowerShell' | Out-Null
+            }
+            catch{
+                write-host $Error[0].exception.message -ForegroundColor Red
+                return
+            }
+        }
+        
+        #Get user pwd.
+        [System.Security.SecureString]$secString = Read-Host "Password" -AsSecureString
+        
+       <# #Remove existing session.
+        Write-Verbose "Removing existing session.."
+        $PSSession = Get-PSSession | Where-Object {$_.ConfigurationName -eq "Microsoft.Exchange"}
+        if($PSSession){
+            Remove-PSSession -Id $PSSession.id -ErrorAction SilentlyContinue
+        }#>
+    }
+    Process
+    {
+        try
+        {
+            #Test emailaddress validity.
+            Write-Verbose "Testing emailaddress validity.."
+            $mail = New-Object System.Net.Mail.MailAddress($emailAddress) -ErrorAction stop
+        }
+        catch
+        {
+            Write-Host "Mail format invalid." -ForegroundColor Red
+            return
+        }
+        
+        $cred = New-Object System.Management.Automation.PSCredential($mail,$secString)
+
+        try{
+            #Connect Sharepoint Online service.
+            Connect-SPOService -Url "https://$($tenant)-admin.sharepoint.com" -Credential $cred
+            $PSSession = $true
+        }
+        catch{
+            Write-Host $Error[0].Exception.Message -ForegroundColor Red
+            return
+        }
+
+       <# try{
+            #Connect to Exchange Online
+            $PSSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic -AllowRedirection -ErrorAction Stop
+            Import-PSSession $PSSession -ErrorAction Stop
+        }
+        catch{
+            write-host $Error[0].Exception.Message -ForegroundColor Red
+            return
+        } #>
+       
+    }
+    End
+    {
+        if($PSSession){
+            Write-Host "`nConnected to $($tenant) Sharepoint Online" -ForegroundColor Yellow
+        }else{
+            Write-Host "Session import failed." -ForegroundColor Red
+        }
+    }
+}
